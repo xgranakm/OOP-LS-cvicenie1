0daf3ab10453203c1eb965e2447f624ad1a536c4
Notebook <xgranakm@stuba.sk>
2022-02-18 11:48:36 +0100

    switch

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,11 +2,16 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int cislo = Integer.parseInt(args[0]);
-        if(cislo == 0) {
-            System.out.println("je to nula");
+        int cislo = 3;
+        switch(cislo) {
+            case 1:
+                System.out.println("cislo je jedna");
+                break;
+            case 2:
+                System.out.println("cislo je dva");
+                break;
+            default:
+                System.out.println("cislo je ine ako jedna a dva");
         }
-        else
-            System.out.println("nie je to nula");
     }
 }
