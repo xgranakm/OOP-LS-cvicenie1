e9390822e2cc227b929313b02a951b439aba62d8
Notebook <xgranakm@stuba.sk>
2022-02-18 11:37:56 +0100

    premenne

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,6 +2,18 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int celeCislo = 5;
+        long celeVacsieCislo = 5L;
+        float desatinneCislo = 5.5f;
+        double desatinneVacsieCislo = 5.5;
+        char znak = 'a';
+        String retazec = "ahoooooj";
+        boolean pravdivostna = true;
+
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
+
+        int zvysok = 2%3;
+        System.out.println(zvysok);
     }
 }
