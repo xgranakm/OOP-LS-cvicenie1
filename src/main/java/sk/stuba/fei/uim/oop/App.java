e8dbe12f44de15e902b15ed5bca60f1e7db89393
Notebook <xgranakm@stuba.sk>
2022-02-18 11:56:42 +0100

    for, funkcia, pole

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,16 +2,17 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int cislo = 3;
-        switch(cislo) {
-            case 1:
-                System.out.println("cislo je jedna");
-                break;
-            case 2:
-                System.out.println("cislo je dva");
-                break;
-            default:
-                System.out.println("cislo je ine ako jedna a dva");
+        int[] pole = new int[10];
+
+        for(int i = 0; i < pole.length; i++) {
+            pole[i] = i;
         }
+
+        for(int i = 0; i < pole.length; i++) {
+             vypis(i);
+        }
+    }
+    public static void vypis(int  a) {
+        System.out.println(a);
     }
 }
