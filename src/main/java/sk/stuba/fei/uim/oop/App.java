504e8878a5e538b0daf5b84bb3cdf46a6186fb60
Notebook <xgranakm@stuba.sk>
2022-02-18 11:44:38 +0100

    if

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,18 +2,11 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int celeCislo = 5;
-        long celeVacsieCislo = 5L;
-        float desatinneCislo = 5.5f;
-        double desatinneVacsieCislo = 5.5;
-        char znak = 'a';
-        String retazec = "ahoooooj";
-        boolean pravdivostna = true;
-
-        float podielFloat = 2/3.0f;
-        System.out.println(podielFloat);
-
-        int zvysok = 2%3;
-        System.out.println(zvysok);
+        int cislo = Integer.parseInt(args[0]);
+        if(cislo == 0) {
+            System.out.println("je to nula");
+        }
+        else
+            System.out.println("nie je to nula");
     }
 }
